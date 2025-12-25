b9d2c608ed8c9aa0d9eb0245a99bb24851b868f8
riyajath-ahamed <riyajatha@gmail.com>
2025-12-25 23:37:38 +0530

    Introduce tsconfig.build.json for improved TypeScript build configuration, update tsconfig.json to refine include/exclude patterns and enable declaration generation, and modify tsup.config.ts to utilize the new tsconfig for better type declaration handling.

--- packages/flags/tsup.config.ts
+++ packages/flags/tsup.config.ts
@@ -3,12 +3,15 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
-  dts: false, // TODO: deafult true
+  dts: {
+    resolve: true,
+  },
   splitting: false,
   treeshake: true,
   outDir: "dist",
   sourcemap: true,
   clean: true,
   external: ["@configkits/core"],
+  tsconfig: "./tsconfig.build.json",
 });
 
